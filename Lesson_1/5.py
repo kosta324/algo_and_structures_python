3a3e07ee562cbb85277699acfb9bfa65cfedff3e
kosta324 <42012286+kosta324@users.noreply.github.com>
2019-05-20 15:51:25 +0300

    Update 5.py

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,12 @@
 #5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
+start_postition_alphabet = ord('a')
+symbol_1 = ord(input(f'Введите первую букву:\n')) - start_postition_alphabet + 1
+symbol_2 = ord(input(f'Введите вторую букву:\n')) - start_postition_alphabet + 1
+if symbol_1 - symbol_2 != 0:
+    result = abs(symbol_1 - symbol_2) - 1
+else:
+    result = 0
+print(f'Позиция первой буквы в алфавите: {symbol_1}\n'
+      f'Позиция второй буквы в алфавите: {symbol_2}\n'
+      f'Между ними букв: {result}\n')
