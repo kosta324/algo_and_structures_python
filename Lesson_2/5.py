bb9ff7c4546fdd1fc978ffae9f905cebe53169ae
kosta324 <42012286+kosta324@users.noreply.github.com>
2019-05-22 11:50:03 +0300

    Update 5.py

--- Lesson_2/5.py
+++ Lesson_2/5.py
@@ -3,3 +3,19 @@
 под номером 32 и заканчивая 127-м включительно.
 Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
 """
+def output(code=32, text='', counter=0):
+    if code == 128:
+        if text != '':
+            print(text)
+        return
+    if counter == 10:
+        print(text)
+        text = ''
+        counter = 0
+    text += f'{code}-{chr(code)}  '
+    code += 1
+    counter += 1
+    return output(code, text, counter)
+
+
+output()
