d12388737929f2fa54a512d18980132a1406d541
kosta324 <42012286+kosta324@users.noreply.github.com>
2019-05-22 11:48:32 +0300

    Update 4.py

--- Lesson_2/4.py
+++ Lesson_2/4.py
@@ -2,3 +2,15 @@
 4.	Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
 Количество элементов (n) вводится с клавиатуры.
 """
+number = int(input('Введите номер элемента:\n'))
+
+
+def summ_of_elements(n, summ=0):
+    if n == 0:
+        return print(f'сумма элементов ряда чисел до элемента №{number} равна: {summ}')
+    summ += 1 / (2 ** (n - 1)) - 2 / (2 ** (n - 1)) * ((n - 1) % 2)
+    n -= 1
+    return summ_of_elements(n, summ)
+
+
+summ_of_elements(number)
