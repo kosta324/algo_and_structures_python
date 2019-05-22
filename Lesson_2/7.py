18b9340e469669712e6c1ea62e508ea1a37930e0
kosta324 <42012286+kosta324@users.noreply.github.com>
2019-05-22 11:51:15 +0300

    Update 7.py

--- Lesson_2/7.py
+++ Lesson_2/7.py
@@ -3,3 +3,15 @@
 натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
 """
+n = int(input('Введите любое натуральное число для проверки равенства: 1+2+...+n = n(n+1)/2: '))
+
+
+def check_expression(n, left=0, counter=1):
+    while counter <= n:
+        left += counter
+        counter += 1
+        return check_expression(n, left, counter)
+    return print(f'Равенство существует? {left == n * (n + 1) / 2}')
+
+
+check_expression(n)
