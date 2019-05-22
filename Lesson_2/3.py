dbe67226e096d37ec8a97469c13f007ce3161600
kosta324 <42012286+kosta324@users.noreply.github.com>
2019-05-22 11:45:57 +0300

    Update 3.py

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,3 +3,18 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+number = int(input('Введите натуральное число:\n'))
+
+
+def reformator(n, un_number=0):
+    if n == 0:
+        return print(f'Число: {number} наоборот: {un_number}')
+    if un_number == 0:
+        un_number = n % 10
+    else:
+        un_number = int(f'{un_number}{n % 10}')
+    n = int((n - n % 10) / 10)
+    return reformator(n, un_number)
+
+
+reformator(number)
