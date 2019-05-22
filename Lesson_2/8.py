0af61afaea965cf8c7e3b76f2ad32d015e68dc49
kosta324 <42012286+kosta324@users.noreply.github.com>
2019-05-22 11:51:50 +0300

    Update 8.py

--- Lesson_2/8.py
+++ Lesson_2/8.py
@@ -3,3 +3,22 @@
  последовательности чисел. Количество вводимых чисел и цифра,
  которую необходимо посчитать, задаются вводом с клавиатуры.
 """
+n = int(input("Введите искомую цифру: "))
+limit = int(input("Сколько будет чисел в последовательности: "))
+counter = 0
+
+
+def searcher(number, digit, entries=0):
+    if number == 0:
+        return entries
+    if number % 10 == digit:
+        entries += 1
+    return searcher(number // 10, digit, entries)
+
+
+for i in range(limit):
+    temp = int(input(f'Введите число #{i + 1}(из {limit}) последовательности: '))
+    counter = searcher(temp, n, counter)
+
+
+print(f'Искомое число в последовательности встречается раз: {counter}')
